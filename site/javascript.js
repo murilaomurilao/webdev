b8178761f513a0a46f37efbb1c7fab6cad86437d
murilo <67709148+murilaomurilao@users.noreply.github.com>
2021-11-28 19:04:46 -0300

    Delete site directory

--- site/javascript.js
+++
@@ -1,6 +0,0 @@
-function randomColor() {
-    var num = Math.floor(Math.random() * 75) + 180;
-    document.body.style.backgroundColor = rgb(0,255,num);
-}
-
-botaoGeral.addEventListener("click",randomColor);
